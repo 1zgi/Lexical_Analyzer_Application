47db360e1a7969e9ff086697476aaee062f00750
Izgi Kanatli <izgikanatli@gmail.com>
2024-03-20 19:15:10 +0300

    Menu created

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 from sys import *
 from Token import *
+from LexerResult import *
+
+token = Token
+lex = LexerResult
+
+tokens = token.parse(Token, "file.txt")
+
+print("1. Call lex()")
+print("2. Show symbol table")
+print("3. Exit")
+
+for i, token in enumerate(tokens):
+    chosen_option = input("Choose one option:")
+    if chosen_option == "3":
+        break
+    elif chosen_option == "2":
+        print()  # symbol_table
+    elif chosen_option == "1":
+        result = lex.lexer(tokens)
+        print(result)
+    else:
+        print("ERROR: invalid option!")
