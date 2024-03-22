70c8b423d35fc0476ba829c6efec2f94dbb10643
Izgi Kanatli <izgikanatli@gmail.com>
2024-03-22 13:05:19 +0300

    modified for symbol table

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from Token import *
 from LexerResult import *
+from SymbolTable import *
 
 token = Token
 tokens = token.parse("file.txt")
 lex = LexerResult(tokens)
+
 
 print("1. Call lex()")
 print("2. Show symbol table")
@@ -14,7 +16,7 @@ for i, token in enumerate(tokens):
     if chosen_option == "3":
         break
     elif chosen_option == "2":
-        print()  # symbol_table
+        print(symbol_table)
     elif chosen_option == "1":
         result = lex.lexer(tokens)
         for obj in result:
